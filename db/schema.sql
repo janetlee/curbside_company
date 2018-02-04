b9c69312226d5c44459336ee97bbd32b69255bd3
Janet Lee <janetjhu96@gmail.com>
2018-02-04 10:37:36 -0800

    Fix driver schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE FreshMuffinDriver;
 
 USE FreshMuffinDriver;
 
-DROP TABLE IF EXISTS `client`;
+DROP TABLE IF EXISTS 'driver';
 
 CREATE TABLE driver (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
@@ -10,9 +10,14 @@ CREATE TABLE driver (
   user_id VARCHAR(100) NOT NULL UNIQUE
 );
 
+DROP TABLE IF EXISTS 'route';
+
 CREATE TABLE route (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   stops VARCHAR(100) NOT NULL,
   eta VARCHAR(100) NOT NULL,
-  user_id INT FOREIGN KEY REFERENCES driver(UserId)
-)
+  driver_id INT,
+    INDEX driver_idx (driver_id),
+    FOREIGN KEY (driver_id)
+    REFERENCES driver(id)
+);
